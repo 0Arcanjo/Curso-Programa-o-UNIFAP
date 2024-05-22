83a96eb00871a1a27da5da2058e61f6df0224409
Addam <136136067+0Arcanjo@users.noreply.github.com>
2024-05-22 11:15:25 -0300

    Add files via upload
    Signed-off-by: Addam <136136067+0Arcanjo@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,7 @@
+let number01 = 10
+let number02 = 10
+let number03 = 10
+let number04 = 10
+let media = (number01+number02+number03+number04)/4
+console.log("<Tabuada>\n")
+console.log("Media: ",media)
